7a2f0168f52e3ade20e874768f55e3aab39ac4ed
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2019-08-04 12:44:02 +0100

    chore: replace pylama with pylint (#4)
    * add mypy

--- tests/test_download_image_pytest.py
+++ tests/test_download_image_pytest.py
@@ -1,15 +1,24 @@
+""" Unit tests for download_image module
+"""
+
 import filecmp
 
 import os
 
 from downloader.download_image import download_image
 
-url = 'https://python.org/static/community_logos/python-logo-master-v3-TM-flattened.png'
+URL = 'https://python.org/static/community_logos/python-logo-master-v3-TM-flattened.png'
 
 
 def test_download_image_should_save_image(tmpdir):
+    """
+    download_image should save the remote image
+    in a given directory
+    :param tmpdir: pytest temporary directory
+    :return: None
+    """
     tmpdir.mkdir('temp')
-    download_image(tmpdir.get_temproot(), url)
+    download_image(tmpdir.get_temproot(), URL)
 
     assert filecmp.cmp(
         f'{tmpdir.get_temproot()}/python-logo-master-v3-TM-flattened.png',
