e61ce8e23378ed5c456d4d6a736e31af7ee60c40
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2019-08-04 10:41:14 +0100

    feat: download_image fun (#3)

---
+++ downloader/download_image.py
@@ -0,0 +1,13 @@
+import os
+
+import requests
+from requests import Response
+
+
+def download_image(loc: str, url: str) -> None:
+    res: Response = requests.get(url)
+    res.raise_for_status()
+    with open(os.path.join(loc, os.path.basename(url).split("?")[0]),
+              'w+b') as image_file:
+        for chunk in res.iter_content(100000):
+            image_file.write(chunk)
