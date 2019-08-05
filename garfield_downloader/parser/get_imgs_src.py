36eafff379005a46284f23ad86d6b0d7be1a63c6
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2019-08-05 19:09:14 +0100

    feat: get_img_href function to grab comics href (#6)

---
+++ garfield_downloader/parser/get_imgs_src.py
@@ -0,0 +1,26 @@
+""" get_imgs_src module
+
+Defines get_imgs_src function
+
+"""
+
+from typing import List
+
+import bs4
+import requests
+from requests import Response
+
+
+def get_imgs_src(url: str, **kwargs) -> List[str]:
+    """
+    Fetches all src attributes from image elements
+    within div with class comic-display
+    :param url: str
+    :param kwargs: ageGated
+    :return: List[str]
+    """
+    res: Response = requests.get(url, cookies={'age-gated': kwargs.get('age_gated')})
+    res.raise_for_status()
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+    comic_el = soup.select('div.comic-display img')
+    return list(map(lambda img: img['src'], comic_el))
---
+++ tests/parser/test_get_img_hrefs.py
@@ -0,0 +1,65 @@
+""" Unit tests for get_imgs_src module
+"""
+
+from httmock import urlmatch, HTTMock
+
+from garfield_downloader.parser.get_imgs_src import get_imgs_src
+
+
+@urlmatch(netloc=r'(.*\.)?garfield\.com$')
+def garfield_mock(url, request) -> str:
+    """
+    Mock HTTP response using HTTMock
+    :param url: HTTPMock url
+    :param request: HTTPMock request
+    :return: str
+    """
+    return """
+    <html>
+        <body>
+            <div class="comic-display">
+                <img src="first_href"></img>
+            </div>
+            <div class="comic-display">
+                <img src="second_href"></img>
+            </div>
+        </body>
+    </html>
+    """
+
+@urlmatch(netloc=r'(.*\.)?garfield\.com$')
+def garfield_empty_mock(url, request) -> str:
+    """
+    Mock HTTP empty response using HTTMock
+    :param url: HTTPMock url
+    :param request: HTTPMock request
+    :return: str
+    """
+    return """
+    <html>
+        <body></body>
+    </html>
+    """
+
+def test_get_imgs_src_should_return_srcs():
+    """
+    get_imgs_src should extract list of src
+    attributes from a website inside dic.comic-display
+    element
+    :return: None
+    """
+    with HTTMock(garfield_mock):
+        found_hrefs = get_imgs_src('https://garfield.com', age_gated='test')
+        expected_hrefs = ["first_href", "second_href"]
+        assert found_hrefs == expected_hrefs
+
+
+def test_get_imgs_src_should_return_empty_list():
+    """
+    get_imgs_src should return empty list if
+    no comics found
+    :return: None
+    """
+    with HTTMock(garfield_empty_mock):
+        found_hrefs = get_imgs_src('https://garfield.com', age_gated='test')
+        assert found_hrefs == []
