4fcc946d55c5b5780c28cee072a54c142909db65
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2019-08-11 08:58:18 +0100

    feat: prepare links in CLI file (#11)

--- garfield_downloader/__main__.py
+++ garfield_downloader/__main__.py
@@ -1,10 +1,11 @@
 """ Main run file containing the CLI
 """
-
 from datetime import date, datetime
 import click
 
+from garfield_downloader.parser.get_comic_links import get_comic_links
 from garfield_downloader.parser.get_confirmation_cookie import get_confirmation_data, get_age_gated
+from garfield_downloader.parser.get_imgs_src import get_imgs_src
 
 DATE_FORMATS = ["%d/%m/%Y", "%d-%m-%Y", "%Y/%m/%d", "%Y-%m-%d"]
 
@@ -18,7 +19,6 @@ DATE_FORMATS = ["%d/%m/%Y", "%d-%m-%Y", "%Y/%m/%d", "%Y-%m-%d"]
               default=str(date.today()),
               help="Ending date of comics to download",
               type=click.DateTime(formats=DATE_FORMATS))
-# pylint: disable=unused-argument
 def main(start_date: datetime, end_date: datetime) -> None:
     """
     Main method instantiating CLI
@@ -28,9 +28,11 @@ def main(start_date: datetime, end_date: datetime) -> None:
     """
     click.secho("Downloading confirmation cookies from https://garfield.com...", fg='red')
     confirmation_data = get_confirmation_data()
-    # pylint: disable=unused-variable
     age_gated = get_age_gated(confirmation_data[0], confirmation_data[1])
     click.secho("Done! Cookie downloaded successfully", fg='green')
+    website_links = get_comic_links(start_date, end_date)
+    raw_img_srcs = map(lambda link: get_imgs_src(link, age_gated=age_gated)[0], website_links)
+    print(list(raw_img_srcs))
 
 
 # pylint: disable=no-value-for-parameter
